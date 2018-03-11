a3cedfadf71659ddb46524979f5b4faa64e18f15
Marcelo Rafael de Brito <marcelorafaeldebrito@gmail.com>
2018-03-11 03:19:16 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,425 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Advogados | Site de teste</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- jquery 3.1.1 -->
+	<script src="js/jquery.js"></script>
+	<!-- cycle plugin jquery -->
+	<script src="js/cycle.js"></script>
+	<!-- semantic ui framework -->
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.min.css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.6/semantic.min.js"></script>
+	<!-- less -->
+	<link rel="stylesheet/less" type="text/css" href="css/style.less">
+	<script src="js/less.js"></script>
+</head>
+<body>
+<div class="section1">
+	<div class="slide">
+		<div class="img1"></div>
+		<div class="img2"></div>
+		<div class="img3"></div>
+		<!-- <img src="img/ideia.jpg" height="800">
+		<img src="img/suit.jpg" height="800">
+		<img src="img/paisagem.jpg" height="800"> -->
+		<!-- <img src="img/suit.jpg"> -->
+	</div>
+	<!-- my pager -->
+	<div class="ui container center aligned" id="pager"></div>
+</div>
+
+
+
+<div class="section2">
+	<div class="ui container">
+		<div class="ui grid">
+			<div class="ui sixteen wide column">
+			<!-- 	<div class="ui segment"> -->
+					<h1 class="ui center aligned header">
+						Benefícios
+					</h1>
+				<!-- </div> -->
+			</div>
+		</div>
+
+
+		<div class="ui grid grid2">
+
+			<div class="four wide computer eight wide tablet sixteen wide mobile column">
+				<div class="primeiro" data-tooltip="Criado por Freepik a partir de Flaticon" data-position="top center">
+					<img src="img/icons/business/banking-1.png">
+				</div>
+				<!-- description -->
+				<div class="ui center aligned header">
+					Economia
+				</div>
+				<div class="ui center aligned sub header">
+					Nós trabalhamos de uma maneira eficaz, trazendo aos nossos clientes flexibilidade na hora de escolher o valor a ser pago.
+				</div>
+			</div>
+
+
+			<div class="four wide computer eight wide tablet sixteen wide mobile column">
+					<div class="segundo" data-tooltip="Criado por Freepik a partir de Flaticon" data-position="top center">
+						<img src="img/icons/business/calculating.png">
+					</div>
+					<!-- description -->
+				<div class="ui center aligned header">
+					Preço certo
+				</div>
+				<div class="ui center aligned sub header">
+					Procuramos fazer a diferença entre os nossos concorrentes, fazendo um preço justo para cada tipo de causa.
+				</div>
+			</div>
+
+
+			<div class="four wide computer eight wide tablet sixteen wide mobile column">
+					<div class="terceiro" data-tooltip="Criado por Freepik a partir de Flaticon" data-position="top center">
+						<img src="img/icons/business/computin.png">
+					</div>
+					<!-- description -->
+				<div class="ui center aligned header">
+					Atendimento online
+				</div>
+				<div class="ui center aligned sub header">
+					Temos profissionais capacitados para bem atendê-lo 24hrs por dia.
+				</div>
+			</div>
+
+
+			<div class="four wide computer eight wide tablet sixteen wide mobile column">
+					<div class="quarto" data-tooltip="Criado por Freepik a partir de Flaticon" data-position="top center">
+						<img src="img/icons/business/rocket-launch.png">
+					</div>
+					<!-- description -->
+				<div class="ui center aligned header">
+					Rapidez
+				</div>
+				<div class="ui center aligned sub header">
+					Não interessa se o Brasil é demorado para resolver causas, nossa equipe sabe como lidar com isso muito bem. Temos umas carats na manga.
+				</div>
+			</div>
+
+
+		</div> <!-- fecha grid2 -->
+
+	</div> <!-- fecha container -->
+
+</div> <!-- fecha section2 -->
+
+<div class="section3">
+	<div class="ui container">
+
+		<div class="ui inverted center aligned header">
+			Feedback de nossos clientes
+		</div>
+
+		<div class="ui center aligned grid">
+			<div class="five wide computer eight wide tablet sixteen wide mobile column">
+				<div class="ui card">
+					<div class="ui image">
+						<img src="img/peoples/john.jpg">
+					</div>
+					<div class="content">
+						<div class="ui header">John Doe</div>
+						<div class="meta">Em: 30/10/2016</div>
+						<div class="description">
+							Fiquei impressionado com a rapidez do seu atendimento! Recomendo a todos que tem uma causa "impossível", porque para eles não é impossível. Tinha uma causa na justiça há 10 anos, que, foi resolvida em meses. Super recomendo! 
+						</div>
+					</div> <!-- fecha content -->
+					<div class="ui extra content">
+						<div class="ui icons">
+							<i class="ui large icon yellow star"></i>
+							<i class="ui large icon yellow star"></i>
+							<i class="ui large icon yellow star"></i>
+							<i class="ui large icon yellow star"></i>
+							<i class="ui large icon yellow star"></i>
+						</div>
+					</div> <!-- fecha extra -->
+				</div> <!-- fecha card -->
+			</div> <!-- fecha column -->
+			<div class="five wide computer eight wide tablet sixteen wide mobile column">
+				<div class="ui card">
+					<div class="ui image">
+						<img src="img/peoples/linden.jpg">
+					</div>
+					<div class="content">
+						<div class="ui header">Washington Borges</div>
+						<div class="meta">Em: 20/9/2016</div>
+						<div class="description">
+							É um grupo de advogados que tem muito futuro pela frente, confio no seus serviços. Obrigado!
+						</div>
+					</div> <!-- fecha content -->
+					<div class="ui extra content">
+						<div class="ui icons">
+							<i class="ui large icon yellow star"></i>
+							<i class="ui large icon yellow star"></i>
+							<i class="ui large icon yellow star"></i>
+							<i class="ui large icon yellow star"></i>
+							<i class="ui large icon yellow star"></i>
+						</div>
+					</div> <!-- fecha extra -->
+				</div> <!-- fecha card -->
+			</div>
+			<div class="five wide computer sixteen wide tablet sixteen wide mobile column">
+				<div class="ui card milli">
+					<div class="ui image">
+						<img src="img/peoples/mili.jpg">
+					</div>
+					<div class="content">
+						<div class="ui header">Emilly silva</div>
+						<div class="meta">Em: 10/11/2016</div>
+						<div class="description">
+							Me atenderam muito bem. Fiquei surpresa coma a rapidez do seu atendimento, de fato é o melhor!
+						</div>
+					</div> <!-- fecha content -->
+					<div class="ui extra content">
+						<div class="ui icons">
+							<i class="ui large icon yellow star"></i>
+							<i class="ui large icon yellow star"></i>
+							<i class="ui large icon yellow star"></i>
+							<i class="ui large icon yellow star"></i>
+							<i class="ui large icon yellow star"></i>
+						</div>
+					</div> <!-- fecha extra -->
+				</div> <!-- fecha card -->
+			</div> <!-- fecha column -->
+		</div> <!-- fecha grid -->
+	</div> <!-- fecha container -->
+</div> <!-- fecha section3 -->
+
+
+<div class="section4">
+	<div class="ui container">
+		<h1 class="ui center aligned inverted header">
+			CONFIE SUAS CAUSAS NA MÃO DE QUEM ENTENDE!
+		</h1>
+	</div> <!-- fecha container -->
+</div> <!-- fecha section4 -->
+
+
+<div class="section5">
+	<div class="ui container">
+		<h1 class="ui inverted center aligned header">
+			Entre em contato
+		</h1>
+
+		<div class="ui center aligned grid">
+			<div class="eight wide computer ten wide tablet sixteen wide mobile column">
+				<div class="div-form">
+					<form class="ui form">
+						<div class="ui two fields">
+							<div class="field">
+								<div class="ui left icon input">
+									<i class="icon user"></i>
+									<input type="text" placeholder="NOME">
+								</div>
+							</div>
+
+							<div class="field">
+								<div class="ui left icon input">
+									<i class="icon mail"></i>
+									<input type="text" placeholder="EMAIL">
+								</div>
+							</div>
+						</div> <!-- two fields -->
+
+						<div class="ui two fields">
+							<div class="field">
+								<div class="ui selection dropdown">
+									<input type="hidden" name="estado">
+									<span class="default text">Digite seu estado</span>
+									<i class="dropdown icon"></i>
+									<div class="ui menu">
+										<div class="item">Teste1</div>
+										<div class="item">Teste2</div>
+										<div class="item">Teste3</div>
+										<div class="item">Teste4</div>
+										<div class="item">Teste5</div>
+										<div class="item">Teste5</div>
+									</div>
+								</div>
+							</div>
+
+							<div class="field">
+								<div class="ui selection dropdown">
+									<input type="hidden" name="sexo">
+									<span class="default text">Sexo</span>
+									<i class="dropdown icon"></i>
+									<div class="ui menu">
+										<div class="item">Masculino</div>
+										<div class="item">Feminino</div>
+									</div>
+								</div>	
+							</div>
+						</div> <!-- fecha two fields -->
+
+						<div class="ui fluid field">
+							<a class="ui inverted basic button">Enviar</a>
+						</div>
+					</form> <!-- fecha form -->
+				</div> <!-- fecha div-form -->
+			</div> <!-- fecha eight column -->
+		</div> <!-- center aligned -->
+
+	</div> <!-- fecha container -->
+
+</div> <!-- fecha section5 -->
+
+
+<div class="section6">
+	<div class="ui container">
+		<div class="ui center aligned grid grid1">
+			<div class="ui compact raised segment">
+				<h1 class="ui center aligend header">
+					Áreas de atuação
+				</h1>
+			</div>
+		</div>
+		<div class="ui center aligned grid grid2">
+			<div class="five wide computer five wide tablet sixteen wide mobile column" 
+			data-tooltip="Criado por Madebyoliver a partir de Flaticon"
+			-position="center top">
+				<div class="ui raised left aligned segment">
+					<div class="ui small left floated image">
+						<img src="img/icons/essencial/briefcase.png">
+					</div>
+					<div class="ui content">
+						<div class="ui header">Direito empresarial</div>
+						<!-- <div class="description">description</div> -->
+					</div>
+				</div> <!-- fecha segment -->
+			</div> <!-- fecha column -->
+			<div class="five wide computer five wide tablet sixteen wide mobile column" 
+			data-tooltip="Criado por Madebyoliver a partir de Flaticon"
+			-position="center top">
+				<div class="ui raised left aligned segment">
+					<div class="ui small left floated image">
+						<img src="img/icons/essencial/home-1.png">
+					</div>
+					<div class="ui content">
+						<div class="ui header">Direito imobiliário</div>
+						<!-- <div class="description">description</div> -->
+					</div>
+				</div> <!-- fecha segment -->
+			</div> <!-- fecha column -->
+			<div class="five wide computer five wide tablet sixteen wide mobile column" 
+			data-tooltip="Criado por Madebyoliver a partir de Flaticon"
+			-position="center top">
+				<div class="ui raised left aligned segment">
+					<div class="ui small left floated image">
+						<img src="img/icons/essencial/user-6.png">
+					</div>
+					<div class="ui content">
+						<div class="ui header">Direito civil</div>
+						<!-- <div class="description">description</div> -->
+					</div>
+				</div> <!-- fecha segment -->
+			</div> <!-- fecha column -->
+			<div class="five wide computer five wide tablet sixteen wide mobile column" 
+			data-tooltip="Criado por Madebyoliver a partir de Flaticon"
+			-position="center top">
+				<div class="ui raised left aligned segment">
+					<div class="ui small left floated image">
+						<img src="img/icons/essencial/notepad-1.png">
+					</div>
+					<div class="ui content">
+						<div class="ui header">Direito contratual</div>
+						<!-- <div class="description">description</div> -->
+					</div>
+				</div> <!-- fecha segment -->
+			</div> <!-- fecha column -->
+			<div class="five wide computer five wide tablet sixteen wide mobile column"
+			data-tooltip="Criado por Freepik a partir de Flaticon"
+			data-position="left center">
+				<div class="ui raised left aligned segment">
+					<div class="ui small left floated image">
+						<img src="img/icons/business/banks.png">
+					</div>
+					<div class="ui content">
+						<div class="ui header">Direito tributário</div>
+						<!-- <div class="description">description</div> -->
+					</div>
+				</div> <!-- fecha segment -->
+			</div> <!-- fecha column -->
+
+		</div> <!-- fecha grid -->
+	</div> <!-- fecha container -->
+</div> <!-- fecha section6 -->
+
+
+
+
+<footer>
+	<div class="ui container">
+		<div class="ui center aligned grid">
+			<div class="eight wide computer eight wide tablet sixteen wide mobile column">
+				<div class="ui segments segments-credito">
+					<div class="ui segment open-credito">
+						<h2 class="ui center aligned header">
+							Créditos(clique)
+						</h2>
+					</div>
+					<div class="ui padded segment esconder">
+						<div class="ui center aligned icon">
+							<i class="large close icon close-credito"></i>
+						</div>
+					</div>
+					<div class="ui segment esconder">
+						Pacote de ícone: business <br>
+						Cofrinho - Calculadora - Mouse - Jato - Banco<br>
+						banking-1.png - calculating.png - computin.png - rocket-launch.png - banks.png <br>
+						"Criado por {Freepik} a partir de Flaticon" 
+					</div>
+					<div class="ui segment esconder">
+						Pacote de ícone: essencial<br>
+						Mala marrom - Casa - Caricatura - Notepad<br>
+						briefcase.png - home-1.png - user-6.png - notepad-1.png<br>
+						"Criado por {Madebyoliver} a partir de Flaticon"
+					</div>
+				</div>
+			</div>
+
+			<!-- <div class="sixteen wide column">
+				<p class="ui center aligned header">
+					O ser humano jamais será perfeito. Se há algum erro, por favor avise-me, obrigado!
+				</p>
+			</div> -->
+		</div>
+	</div>
+</footer>
+
+
+
+
+<script type="text/javascript">
+$(document).ready(function() {
+	$('.slide').cycle({
+		fx:       'fade',
+		speedIn:   1000,
+		speedOut:  1000,
+		timeout:   10,
+		sync:      true,
+		pager:     '#pager'
+	});
+
+	//addclas semantic ui to my pager
+	$('#pager > a').addClass('ui inverted grey huge button');
+
+	$('.ui.dropdown').dropdown();
+
+
+	$('footer .segments-credito').children('.ui.segment').css("display", "none").first().css("display", "block");
+	$('footer .segments-credito .open-credito').click(function() {
+		$('footer .segments-credito').children('.ui.segment').slideToggle(800);
+	});
+	$('.close-credito').click(function() {
+		$('footer .segments-credito').children('.ui.segment').slideToggle(800);
+	});
+});
+</script>
+
+</body>
+</html>
